d2067c408d50bc5c4eee2587f1462de80a637e5c
Hannah McCain <hannahmccain@Hannahs-MacBook-Air.local>
2023-01-10 15:35:12 -0700

    Add classnames lib and add nav button.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import classNames from "classnames";
 
 const IndexPage: React.FC<PageProps> = () => {
+  const [showNav, setShowNav] = React.useState(false);
   return (
     <main className="grid gap-4 grid-cols-3 grid-rows-3 min-h-screen">
-      <h1 className="text-orange-600 mt-16 text-7xl col-start-2 row-start-2 text-center">
+      <button
+        className="text-orange-600 mb-36 text-7xl col-start-2 row-start-2 text-center"
+        onClick={() => setShowNav(true)}>
         hannah joyce
-      </h1>
+      </button>
+      <button
+        className={classNames(
+          "col-start-1 row-start-3",
+          showNav ? "block" : "hidden"
+        )}>
+        menu
+      </button>
     </main>
   );
 };
