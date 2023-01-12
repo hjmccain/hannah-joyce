f03ada0f06a2d02c6d676a1f7fc0cc5078aa1c79
Hannah McCain <hannahmccain@Hannahs-MacBook-Air.local>
2023-01-12 15:22:58 -0700

    Add open-close carets.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,23 +14,34 @@ const IndexPage: React.FC<PageProps> = () => {
   // get screen size and set `showNav` to true below 768px
 
   return (
-    <main className="grid min-h-screen grid-rows-[repeat(3,_fit-content(100px))] gap-4 md:grid-cols-3 md:grid-rows-3">
+    <main className="grid min-h-screen grid-rows-[repeat(3,_fit-content(100px))] gap-4 md:grid-rows-3 xl:grid-cols-3">
       <div
         className={classNames(
-          showAboutMe ? "md:h-[416px]" : "md:h-[144px]",
-          "flex h-fit w-fit flex-col overflow-hidden transition-all duration-300 md:col-start-2 md:row-span-2 md:row-start-2 md:mx-auto md:items-center md:rounded-3xl md:border-4 md:border-black md:bg-slate-100 md:p-6"
+          showAboutMe ? "md:h-[446px]" : "md:h-[174px]",
+          "flex h-fit w-fit flex-col overflow-hidden transition-all duration-500 md:col-start-1 md:row-span-2 md:row-start-2 md:mx-auto md:items-center md:rounded-[100px] md:border-4 md:border-black md:bg-slate-100 md:p-10 xl:col-start-2"
         )}>
         <button
           className="m-12 my-4 h-10 text-left text-orange-600 md:m-0 md:mb-4 md:h-auto md:text-center md:text-7xl"
           onClick={() => setShowNav(true)}>
           hannah joyce
         </button>
+        <button
+          className={classNames(
+            showAboutMe
+              ? "text-[0px] opacity-0"
+              : "mt-[-.5rem] text-xl opacity-100",
+            "hidden self-center transition-opacity delay-200 md:block"
+          )}
+          onClick={handleSetShowAboutMe}>
+          ⌄
+        </button>
+
         <div
           className={classNames(
             showAboutMe
               ? "bg-slate-100 p-10 text-sm opacity-100 md:p-0"
               : "text-[0px] opacity-0",
-            "flex max-w-[400px] flex-col items-end transition-all md:m-0 md:mb-4"
+            "flex flex-col items-end transition-all duration-300 md:m-0 md:w-[420px]"
           )}>
           <button className="md:hidden" onClick={handleSetShowAboutMe}>
             X
@@ -42,6 +53,11 @@ const IndexPage: React.FC<PageProps> = () => {
             veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
             ea commodo consequat.
           </p>
+          <button
+            className="mt-4 hidden self-center text-xl md:block"
+            onClick={handleSetShowAboutMe}>
+            ^
+          </button>
         </div>
       </div>
       {showNav && (
