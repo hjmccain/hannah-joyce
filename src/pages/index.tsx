47e0f1f556f89fae4eaccce5685fd9639cd936f1
Hannah McCain <hannahmccain@Hannahs-MacBook-Air.local>
2023-01-12 14:08:01 -0700

    Add bio placeholder and transitions.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,47 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import Nav from "./nav";
 import classNames from "classnames";
 
 const IndexPage: React.FC<PageProps> = () => {
   const [showNav, setShowNav] = React.useState(false);
+  const [showAboutMe, setShowAboutMe] = React.useState(false);
+
+  const handleSetShowAboutMe = () => {
+    setShowAboutMe(!showAboutMe);
+  };
 
   // get screen size and set `showNav` to true below 768px
 
   return (
     <main className="grid min-h-screen grid-rows-[repeat(3,_fit-content(100px))] gap-4 md:grid-cols-3 md:grid-rows-3">
-      <button
-        className="ml-12 mt-4 h-10 text-left text-orange-600 md:col-start-2 md:row-start-2 md:mt-[-6rem] md:ml-0 md:h-full md:text-center md:text-7xl"
-        onClick={() => setShowNav(true)}>
-        hannah joyce
-      </button>
-      {showNav && (
-        <div className="flex items-end md:col-start-1 md:row-start-3 md:mb-16">
-          <nav className="nav group relative h-fit content-end md:row-start-3 md:m-10">
-            <button className="absolute bottom-[-66px] ml-10 rounded-full border-4 border-black bg-white py-6 px-4 opacity-100 duration-1000 group-hover:opacity-0 md:ml-0 md:py-10 md:px-8">
-              menu
-            </button>
-            <div className="mt-4 opacity-0 group-hover:opacity-100 md:mt-0">
-              <button
-                className={classNames(
-                  "absolute bottom-[-66px] z-20 ml-10 w-28 origin-[40px_40px] rounded-full border-4 border-black bg-white py-6 hover:bg-slate-400 group-hover:w-72 group-hover:rotate-[65deg] md:ml-0 md:origin-[56px_56px] md:py-10 md:group-hover:w-96 md:group-hover:-rotate-[65deg]"
-                )}>
-                about
-              </button>
-              <button
-                className={classNames(
-                  "absolute bottom-[-66px] z-10 ml-10 w-28 origin-[40px_40px] rounded-full border-4 border-black bg-white py-6 hover:bg-slate-400 group-hover:w-72 group-hover:rotate-[32deg] md:ml-0 md:origin-[56px_56px] md:py-10 md:group-hover:w-96 md:group-hover:-rotate-[32deg]"
-                )}>
-                gallery
-              </button>
-              <button className="absolute bottom-[-66px] ml-10 w-28 origin-[40px_40px] rounded-full border-4 border-black bg-white py-6 duration-1000 hover:bg-slate-400 group-hover:w-72 md:ml-0 md:origin-[56px_56px] md:py-10 md:group-hover:w-96">
-                contact
-              </button>
-            </div>
-          </nav>
+      <div
+        className={classNames(
+          showAboutMe ? "h-[416px]" : "h-[144px]",
+          "flex w-fit flex-col overflow-hidden transition-all duration-300 md:col-start-2 md:row-span-2 md:row-start-2 md:mx-auto md:items-center md:rounded-3xl md:border-4 md:border-black md:bg-slate-100 md:p-6"
+        )}>
+        <button
+          className="m-12 my-4 h-10 text-left text-orange-600 md:m-0 md:mb-4 md:h-auto md:text-center md:text-7xl"
+          onClick={() => setShowNav(true)}>
+          hannah joyce
+        </button>
+        {/* {showAboutMe && ( */}
+        <div
+          className={classNames(
+            showAboutMe ? "opacity-100" : "opacity-0",
+            "flex max-w-[400px] items-end transition-opacity md:mb-4"
+          )}>
+          <p className="text-justify md:text-2xl">
+            is a software developer and poet who bleh blah blo blu blah ipsum
+            dolor sit amet, con sectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+          </p>
         </div>
-      )}
+        {/* )} */}
+      </div>
+      {showNav && <Nav toggleAbout={handleSetShowAboutMe} />}
     </main>
   );
 };
